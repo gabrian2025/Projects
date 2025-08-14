cd8a6113a1a3989eb9d53ebe38e5d2b427eec84a
gabrian2025 <gabrian@vt.edu>
2025-08-14 14:03:27 -0400

    Update Player.java

--- tennis_players/src/main/java/com/tp/tennis_players/player/Player.java
+++ tennis_players/src/main/java/com/tp/tennis_players/player/Player.java
@@ -1,4 +1,23 @@
 package com.tp.tennis_players.player;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="player_statistic")
 public class Player {
+    @Id
+    @Column(name="player_name" , unique=true)
+    private String name;
+    private Integer rank;
+    private Integer winPercentage;
+    private Double matchesPlayed;
+
+    public Player()
+    {
+        
+    }
+
 }
